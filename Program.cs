daffd0a4750de9df2e39d3e86ac81428da27f33b
Anacarolinar21 <anacarolinarodriguescarolina2@gmail.com>
2022-03-18 21:41:39 -0300

    finalizar nomecompleto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string nome, sobrenome, SOBRENOME;
+
+Console.WriteLine("digite seub primeiro nome:");
+nome = Console.ReadLine()!;
+
+Console.WriteLine("digite seu sobrenome:");
+sobrenome = Console.ReadLine()!;
+
+SOBRENOME=sobrenome.ToUpper();
+
+Console.WriteLine ($"nome completo: {nome} {sobrenome}"!);
+Console.WriteLine($"nome de catalogo: {SOBRENOME} {nome} "!);
